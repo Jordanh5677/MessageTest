3ce01ee46c246f83c995a2b80ae338126e326eb4
Rio6 <rio6.liu@gmail.com>
2017-10-19 21:17:45 -0700

    Use GridBagLayout in OptionsFrame

--- src/com/meme/jordan/messageTestJava/OptionsFrame.java
+++ src/com/meme/jordan/messageTestJava/OptionsFrame.java
@@ -5,6 +5,12 @@ import java.awt.*;
 
 public class OptionsFrame extends JFrame {
     public OptionsFrame(MainFrame main) {
+
+        GridBagLayout layout = new GridBagLayout();
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.insets = new Insets(3, 3, 3, 3);
+
         JTextField nameField = new JTextField();
         JCheckBox serverCheck = new JCheckBox();
         JTextField ipField = new JTextField();
@@ -14,19 +20,47 @@ public class OptionsFrame extends JFrame {
         JButton okButton = new JButton("OK");
         JButton cancelButton = new JButton("Cancel");
 
+        gbc.weightx = 0;
+        gbc.gridwidth = GridBagConstraints.RELATIVE;
+        layout.setConstraints(nameLabel, gbc);
         add(nameLabel);
+
+        gbc.weightx = 1;
+        gbc.gridwidth = GridBagConstraints.REMAINDER;
+        layout.setConstraints(nameField, gbc);
         add(nameField);
+
+        gbc.weightx = 0;
+        gbc.gridwidth = GridBagConstraints.RELATIVE;
+        layout.setConstraints(serverLabel, gbc);
         add(serverLabel);
+
+        gbc.weightx = 1;
+        gbc.gridwidth = GridBagConstraints.REMAINDER;
+        layout.setConstraints(serverCheck, gbc);
         add(serverCheck);
+
+        gbc.weightx = 0;
+        gbc.gridwidth = GridBagConstraints.RELATIVE;
+        layout.setConstraints(ipLabel, gbc);
         add(ipLabel);
+
+        gbc.weightx = 1;
+        gbc.gridwidth = GridBagConstraints.REMAINDER;
+        layout.setConstraints(ipField, gbc);
         add(ipField);
+
+        gbc.gridwidth = GridBagConstraints.RELATIVE;
+        layout.setConstraints(okButton, gbc);
         add(okButton);
+
+        gbc.gridwidth = GridBagConstraints.RELATIVE;
+        layout.setConstraints(cancelButton, gbc);
         add(cancelButton);
 
-        setLayout(new GridLayout(0, 2));
-        setSize(300,200);
+        setLayout(layout);
+        setSize(300, 150);
         setResizable(false);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
     public void showFrame(){
